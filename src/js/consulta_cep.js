b4b15f1f0cd379a1e5cc8feb42d7983ab2aa4c8e
adrianojsouza <adrianojdesouza@gmail.com>
2015-07-07 21:03:11 -0300

    Adicionado mascaras e validações para o campo do cep

--- src/js/consulta_cep.js
+++ src/js/consulta_cep.js
@@ -11,6 +11,11 @@ angular.module('searchCep', [])
         'uf': ''
     };
 
+    $scope.btnConsultaIsDisabled = true;
+
+    /**
+     * Consulta no webservice
+     */
     $scope.doSearch = function() {
 
         var cep = $scope.dados.cep;
@@ -43,11 +48,32 @@ angular.module('searchCep', [])
         });
     };
 
+    /**
+     * Reinicia os valores padrões antes da consulta
+     */
     $scope.doReset = function() {
         $scope.dados.logradouro = '';
         $scope.dados.bairro = '';
         $scope.dados.cidade = '';
         $scope.dados.uf = '';
+    };
+
+    $scope.setMaskCep = function() {
+
+        var patt = new RegExp('([0-9]{5})([0-9]{1,})');
+
+        if (patt.test($scope.dados.cep)) {
+            var tmp = $scope.dados.cep.replace(new RegExp('[^0-9]'), '');
+            $scope.dados.cep = tmp.replace(patt, "$1-$2");
+        }
+
+        // Formato do cep é valido habilita o botãos
+        if ((new RegExp('[0-9]{5}-[0-9]{3}')).test($scope.dados.cep)) {
+            $scope.btnConsultaIsDisabled = false;
+            return;
+        }
+
+        $scope.btnConsultaIsDisabled = true;
     };
 
 });
